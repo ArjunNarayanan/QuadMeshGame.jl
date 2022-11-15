c6c5cc8cc2eaab567bc7644491f5b2f22f480d34
Arjun Narayanan <arjun.narayanan91@gmail.com>
2022-11-15 12:18:59 -0700

    tiny change

--- src/mesh.jl
+++ src/mesh.jl
@@ -467,7 +467,7 @@ end
 
 function vertex_degrees(edges, num_vertices)
     degrees = zeros(Int, num_vertices)
-    for (i, _) in enumerate(eachcol(edges))
+    for (i, col) in enumerate(eachcol(edges))
         degrees[edges[:, i]] .+= 1
     end
     return degrees
