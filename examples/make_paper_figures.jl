46f21d751b2a0e3d2d15369369ba1913e0a2ca8f
Arjun Narayanan <arjun.narayanan91@gmail.com>
2023-08-21 10:00:28 -0700

    cookie

--- examples/make_paper_figures.jl
+++ examples/make_paper_figures.jl
@@ -4,15 +4,17 @@ using PlotQuadMesh
 QM = QuadMeshGame
 PQ = PlotQuadMesh
 
-function plot_mesh(mesh; vertex_size = 70, fontsize = 60)
+function plot_mesh(mesh; vertex_size = 10, fontsize = 60)
     fig, ax = PQ.plot_mesh(
         QM.active_vertex_coordinates(mesh), 
         QM.active_quad_connectivity(mesh), 
         number_vertices=true, 
-        number_elements=true, 
+        number_elements=false, 
         # internal_order=true, 
         fontsize = fontsize,
-        vertex_size = vertex_size)
+        vertex_size = vertex_size^2,
+        figsize=(20,20)
+    )
     fig.tight_layout()
     return fig
 end
@@ -24,14 +26,17 @@ connectivity = [1 3 4 2
                 3 5 6 4]
 mesh = QM.QuadMesh(vertices, connectivity')
 fig = plot_mesh(mesh)
-fig.savefig("examples/paper_figures/flips/flip-0.png")
+fig.tight_layout()
+fig.savefig("examples/paper_figures/flips/flip-0.pdf")
 QM.left_flip!(mesh, 1, 2)
 fig = plot_mesh(mesh)
-fig.savefig("examples/paper_figures/flips/left-flip-0.png")
+fig.tight_layout()
+fig.savefig("examples/paper_figures/flips/left-flip-0.pdf")
 QM.right_flip!(mesh, 1, 2)
 QM.right_flip!(mesh, 1, 2)
 fig = plot_mesh(mesh)
-fig.savefig("examples/paper_figures/flips/right-flip-0.png")
+fig.tight_layout()
+fig.savefig("examples/paper_figures/flips/right-flip-0.pdf")
 ##
 
 ##
@@ -47,27 +52,32 @@ connectivity = [
 ]
 mesh = QM.QuadMesh(vertices, connectivity')
 fig = plot_mesh(mesh)
-fig.savefig("examples/paper_figures/split-collapse/split-0.png")
+fig.tight_layout()
+fig.savefig("examples/paper_figures/split-collapse/split-0.pdf")
 QM.split!(mesh, 1, 3)
 QM.averagesmoothing!(mesh)
 fig = plot_mesh(mesh)
-fig.savefig("examples/paper_figures/split-collapse/split-1.png")
-# mesh = QM.QuadMesh(vertices, connectivity')
-# QM.split!(mesh, 2, 4)
-# fig = plot_mesh(mesh)
-# fig.savefig("examples/paper_figures/split-2.png")
+fig.tight_layout()
+fig.savefig("examples/paper_figures/split-collapse/split-1.pdf")
+mesh = QM.QuadMesh(vertices, connectivity')
+QM.split!(mesh, 2, 4)
+fig = plot_mesh(mesh)
+fig.tight_layout()
+fig.savefig("examples/paper_figures/split-collapse/split-2.pdf")
 ##
 
 
 ##
 mesh = QM.square_mesh(2)
 fig = plot_mesh(mesh, fontsize = 50)
-fig.savefig("examples/paper_figures/global-split/global-split-0.png")
+fig.tight_layout()
+fig.savefig("examples/paper_figures/global-split/global-split-0.pdf")
 tracker = QM.Tracker()
 QM.global_split_without_loops!(mesh, 2, 1, tracker, 10)
-QM.averagesmoothing!(mesh)
+# QM.averagesmoothing!(mesh)
 fig = plot_mesh(mesh, fontsize = 50)
-fig.savefig("examples/paper_figures/global-split/global-split-1.png")
+fig.tight_layout()
+fig.savefig("examples/paper_figures/global-split/global-split-1.pdf")
 ##
 
 ##
@@ -78,10 +88,10 @@ mesh = QM.QuadMesh(QM.active_vertex_coordinates(mesh),
                    QM.active_quad_connectivity(mesh),
                    is_geometric_vertex=is_geometric_vertex)
 fig = plot_mesh(mesh)
-fig.savefig("examples/paper_figures/global-cleanup/cleanup-0.png")
+fig.savefig("examples/paper_figures/global-cleanup/cleanup-0.pdf")
 tracker = QM.Tracker()
 QM.cleanup_path!(mesh, 1, 2, 10, tracker)
 QM.reindex!(mesh)
 fig = plot_mesh(mesh)
-fig.savefig("examples/paper_figures/global-cleanup/cleanup-1.png")
+fig.savefig("examples/paper_figures/global-cleanup/cleanup-1.pdf")
 ##
